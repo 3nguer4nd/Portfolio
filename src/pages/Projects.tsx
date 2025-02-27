6fdd09265be3e7306e139e04aea6c4e911360aad
Enguérand.CA <enguerand.ca@gmail.com>
2025-02-27 15:49:21 +0100

    commit

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -9,7 +9,7 @@ function Projects() {
       image: "https://images.unsplash.com/photo-1558494949-ef010cbdcc31?ixlib=rb-4.0.3&auto=format&fit=crop&w=1950&q=80",
       tags: ["Git", "Web", "Bolt"],
       githubLink: "https://github.com/3nguer4nd/Portfolio",
-      demoLink: "enguerand-ca.fr"
+      demoLink: "https://enguerand-ca.fr"
     },
     {
       title: "Plateforme de gestion de tickets",
