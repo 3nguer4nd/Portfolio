97780ab750c660b5f61f5082966b7df722e3cdfc
Enguérand.CA <enguerand.ca@gmail.com>
2025-02-27 16:10:38 +0100

    commit

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -6,7 +6,7 @@ function Projects() {
     {
       title: "Créer et Héberger un portfolio",
       description: "Hébergement d'un Portfolio en ligne grâce à des outils comme Vercel, GitHub ou OVH.",
-      image: "https://images.unsplash.com/photo-1558494949-ef010cbdcc31?ixlib=rb-4.0.3&auto=format&fit=crop&w=1950&q=80",
+      image: "portfolioPNG.png",
       tags: ["Git", "Web", "Bolt"],
       githubLink: "https://github.com/3nguer4nd/Portfolio",
       demoLink: "https://enguerand-ca.fr"
