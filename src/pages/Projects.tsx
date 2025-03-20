669513ac09dd1160385bfd4a9c97697ff3466582
Enguérand.CA <enguerand.ca@gmail.com>
2025-03-20 17:51:06 +0100

    commit

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -66,7 +66,7 @@ function Projects() {
           
           <div className="mt-6 flex items-center justify-center space-x-4">
             <a
-              href="/path/to/your/cv.pdf"
+              href="/portfolio\Tab E5.pdf"
               target="_blank"
               rel="noopener noreferrer"
               className="inline-flex items-center px-4 py-2 border border-primary rounded-md shadow-sm text-sm font-medium text-white bg-dark-light hover:bg-dark-lighter transition-colors"
